e69342f00cd5c99856b350817c58315458cae860
James Li <lijamez@users.noreply.github.com>
2017-08-12 00:26:08 -0700

    Made TonbotPlugin an abstract class, so extensions can have default implementations.

--- src/main/java/com/tonberry/tonbot/common/TonbotPlugin.java
+++ src/main/java/com/tonberry/tonbot/common/TonbotPlugin.java
@@ -1,42 +1,49 @@
 package com.tonberry.tonbot.common;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.util.Set;
 
 /**
- * Tonbot uses classes of this type to instantiate {@link PluginResources}s.
  * The Tonbot plugin loader will create an instance of this class using a 1-parameter constructor which accepts a
  * {@link TonbotPluginArgs}.
  */
-public interface TonbotPlugin {
+public abstract class TonbotPlugin {
 
     /**
      * Gets the human friendly name of the plugin.
      * @return The human friendly name of the plugin. Non-null.
      */
-    String getFriendlyName();
+    abstract String getFriendlyName();
 
     /**
      * Gets a short description of what this plugin does. This resulting sentence should be grammatically correct when
      * preceded by the words, "This plugin can..."
      * @return The action description. Non-null.
      */
-    String getActionDescription();
+    abstract String getActionDescription();
 
     /**
      * Whether if this plugin should be hidden from the "help" menu.
      * @return True if this plugin should be hidden from the help menu. False otherwise.
      */
-    boolean isHidden();
+    boolean isHidden() {
+        return false;
+    }
 
     /**
      * Gets the set of {@link Activity}.
      * @return The set of {@link Activity}. Non-null.
      */
-    Set<Activity> getActivities();
+    Set<Activity> getActivities() {
+        return ImmutableSet.of();
+    }
 
     /**
      * Gets the set of {@link PeriodicTask}.
      * @return The set of {@link Activity}. Non-null.
      */
-    Set<PeriodicTask> getPeriodicTasks();
+    Set<PeriodicTask> getPeriodicTasks() {
+        return ImmutableSet.of();
+    }
 }
