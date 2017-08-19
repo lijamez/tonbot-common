018f99599a9402865d01131937a915ddbc4bfa7f
James Li <lijamez@users.noreply.github.com>
2017-08-19 11:43:55 -0700

    BotUtils returns Futures, allowing for synchronous message sending.

--- src/main/java/net/tonbot/common/BotUtils.java
+++ src/main/java/net/tonbot/common/BotUtils.java
@@ -7,29 +7,31 @@ import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.RequestBuffer;
+import sx.blah.discord.util.RequestBuffer.RequestFuture;
 
 public class BotUtils {
-    private static final Logger LOG = LoggerFactory.getLogger(BotUtils.class);
+	private static final Logger LOG = LoggerFactory.getLogger(BotUtils.class);
 
-    private BotUtils() { }
+	private BotUtils() {
+	}
 
-    public static void sendMessage(IChannel channel, String message) {
-        RequestBuffer.request(() -> {
-            try {
-                channel.sendMessage(message);
-            } catch (DiscordException e) {
-                LOG.error("Message could not be sent.", e);
-            }
-        });
-    }
+	public static RequestFuture<Void> sendMessage(IChannel channel, String message) {
+		return RequestBuffer.request(() -> {
+			try {
+				channel.sendMessage(message);
+			} catch (DiscordException e) {
+				LOG.error("Message could not be sent.", e);
+			}
+		});
+	}
 
-    public static void sendEmbeddedContent(IChannel channel, EmbedObject embedObject) {
-        RequestBuffer.request(() -> {
-            try {
-                channel.sendMessage(embedObject);
-            } catch (DiscordException e) {
-                LOG.error("Embedded object could not be sent.", e);
-            }
-        });
-    }
+	public static RequestFuture<Void> sendEmbeddedContent(IChannel channel, EmbedObject embedObject) {
+		return RequestBuffer.request(() -> {
+			try {
+				channel.sendMessage(embedObject);
+			} catch (DiscordException e) {
+				LOG.error("Embedded object could not be sent.", e);
+			}
+		});
+	}
 }
