a7ce513442a6cc37244e77d7d4b51dc70b8dfe9a
James Li <lijamez@users.noreply.github.com>
2017-08-20 13:06:15 -0700

    BotUtils is no longer static. This is a backwards incompatible change.

--- src/main/java/net/tonbot/common/BotUtils.java
+++ src/main/java/net/tonbot/common/BotUtils.java
@@ -1,37 +1,60 @@
 package net.tonbot.common;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.common.base.Preconditions;
+import com.google.inject.Inject;
 
+import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.RequestBuffer;
-import sx.blah.discord.util.RequestBuffer.RequestFuture;
+import sx.blah.discord.util.RequestBuilder;
 
+/**
+ * A helper class to send data to discord. Thread safe.
+ */
 public class BotUtils {
-	private static final Logger LOG = LoggerFactory.getLogger(BotUtils.class);
 
-	private BotUtils() {
+	private final IDiscordClient discordClient;
+	
+	@Inject
+	public BotUtils(IDiscordClient discordClient) {
+		this.discordClient = Preconditions.checkNotNull(discordClient, "discordClient must be non-null.");
 	}
 
-	public static RequestFuture<Void> sendMessage(IChannel channel, String message) {
-		return RequestBuffer.request(() -> {
-			try {
+	/**
+	 * Asynchronously sends a message. Will retry if a RateLimitException is returned by Discord.  
+	 * @param channel The channel to send the message to. Non-null.
+	 * @param message The message to be sent. Must be non-null, non-empty.
+	 */
+	public void sendMessage(IChannel channel, String message) {
+		Preconditions.checkNotNull(channel, "channel must be non-null.");
+		Preconditions.checkNotNull(message, "message must be non-null.");
+		
+		new RequestBuilder(discordClient)
+			.shouldBufferRequests(true)
+			.setAsync(true)
+			.doAction(() -> {
 				channel.sendMessage(message);
-			} catch (DiscordException e) {
-				LOG.error("Message could not be sent.", e);
-			}
-		});
+				return true;
+			})
+			.execute();
 	}
 
-	public static RequestFuture<Void> sendEmbeddedContent(IChannel channel, EmbedObject embedObject) {
-		return RequestBuffer.request(() -> {
-			try {
-				channel.sendMessage(embedObject);
-			} catch (DiscordException e) {
-				LOG.error("Embedded object could not be sent.", e);
-			}
-		});
+	/**
+	 * Asynchronously sends an embed object. Will retry if RateLimitException is returned by Discord.
+	 * @param channel The channel to send the message to. Non-null.
+	 * @param embedObj The embed object. Non-null.
+	 */
+	public void sendEmbed(IChannel channel, EmbedObject embedObj) {
+		Preconditions.checkNotNull(channel, "channel must be non-null.");
+		Preconditions.checkNotNull(embedObj, "embedObj must be non-null.");
+		
+		new RequestBuilder(discordClient)
+				.shouldBufferRequests(true)
+				.setAsync(true)
+				.doAction(() -> {
+					channel.sendMessage(embedObj);
+					return true;
+				})
+				.execute();
 	}
 }
--- src/main/java/net/tonbot/common/TonbotPluginArgs.java
+++ src/main/java/net/tonbot/common/TonbotPluginArgs.java
@@ -19,11 +19,13 @@ public class TonbotPluginArgs {
     private final String prefix;
     private final IDiscordClient discordClient;
     private final File configFile;
+    private final BotUtils botUtils;
 
-    private TonbotPluginArgs(String prefix, IDiscordClient discordClient, File configFile) {
+    private TonbotPluginArgs(String prefix, IDiscordClient discordClient, File configFile, BotUtils botUtils) {
         this.prefix = Preconditions.checkNotNull(prefix, "prefix must be non-null.");
         this.discordClient = Preconditions.checkNotNull(discordClient, "discordClient must be non-null.");
         this.configFile = configFile;
+        this.botUtils = Preconditions.checkNotNull(botUtils, "botUtils must be non-null.");
     }
 
     public Optional<File> getConfigFile() {
