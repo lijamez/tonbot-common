43e094ec43c6696479d2e1604a7e56b6ce182e14
James Li <lijamez@users.noreply.github.com>
2017-08-26 14:23:22 -0700

    Added sendMessageSync to BotUtils interface.

--- src/main/java/net/tonbot/common/BotUtils.java
+++ src/main/java/net/tonbot/common/BotUtils.java
@@ -2,6 +2,7 @@ package net.tonbot.common;
 
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
 
 /**
  * A helper class to send data to discord. Thread safe.
@@ -18,6 +19,20 @@ public interface BotUtils {
 	 *            The message to be sent. Must be non-null, non-empty.
 	 */
 	void sendMessage(IChannel channel, String message);
+
+	/**
+	 * Synchronously sends a message. Will retry if a RateLimitException is returned
+	 * by Discord.
+	 * 
+	 * @param channel
+	 *            The channel to send the message to. Non-null.
+	 * @param message
+	 *            The message to be sent. Must be non-null, non-empty.
+	 * @return The {@link IMessage}.
+	 * @throws TonbotTechnicalFault
+	 *             if the message could not be sent.
+	 */
+	IMessage sendMessageSync(IChannel channel, String message);
 
 	/**
 	 * Asynchronously sends an embed object. Will retry if RateLimitException is
