44cc150d7a90a27dfaefd769d7549cf038465ad6
James Li <lijamez@users.noreply.github.com>
2017-10-14 11:24:23 -0700

    Add color support.

--- src/main/java/net/tonbot/common/TonbotPluginArgs.java
+++ src/main/java/net/tonbot/common/TonbotPluginArgs.java
@@ -1,5 +1,6 @@
 package net.tonbot.common;
 
+import java.awt.Color;
 import java.io.File;
 
 import com.google.common.base.Preconditions;
@@ -21,11 +22,18 @@ public class TonbotPluginArgs {
 	private final IDiscordClient discordClient;
 	private final File configFile;
 	private final BotUtils botUtils;
+	private final Color color;
 
-	private TonbotPluginArgs(String prefix, IDiscordClient discordClient, File configFile, BotUtils botUtils) {
+	private TonbotPluginArgs(
+			String prefix, 
+			IDiscordClient discordClient, 
+			File configFile, 
+			BotUtils botUtils,
+			Color color) {
 		this.prefix = Preconditions.checkNotNull(prefix, "prefix must be non-null.");
 		this.discordClient = Preconditions.checkNotNull(discordClient, "discordClient must be non-null.");
 		this.configFile = Preconditions.checkNotNull(configFile, "configFile must be non-null.");
 		this.botUtils = Preconditions.checkNotNull(botUtils, "botUtils must be non-null.");
+		this.color = Preconditions.checkNotNull(color, "color must be non-null.");
 	}
 }
