39e09b18e24629fa6de22e4eb3020e63376a282c
James Li <lijamez@users.noreply.github.com>
2018-02-14 00:21:50 -0800

    asd

--- src/main/java/net/tonbot/common/Activity.java
+++ src/main/java/net/tonbot/common/Activity.java
@@ -22,6 +22,6 @@ public interface Activity {
 	 *             If a user error occurred.
 	 */
 	public default void enact(MessageReceivedEvent event, String args) {
-		
+
 	}
 }
--- src/main/java/net/tonbot/common/Enactable.java
+++ src/main/java/net/tonbot/common/Enactable.java
@@ -6,8 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates to Tonbot that the annotated method should be invoked instead of the Activity's standard enact method.
- * This annotation should be used on a method with two parameters: MessageReceivedEvent and an object to map arguments to.
+ * Indicates to Tonbot that the annotated method should be invoked instead of
+ * the Activity's standard enact method. This annotation should be used on a
+ * method with a MessageReceivedEvent parameter and optionally, an object to map
+ * arguments to.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
