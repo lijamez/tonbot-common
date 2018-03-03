0413336324c09986e0c28fab36b7e66ec4809aa4
James Li <lijamez@users.noreply.github.com>
2018-03-02 19:50:55 -0800

    BotUtils now supports deleting messages/embeds after some time.

--- src/main/java/net/tonbot/common/BotUtils.java
+++ src/main/java/net/tonbot/common/BotUtils.java
@@ -2,6 +2,7 @@ package net.tonbot.common;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
@@ -38,6 +39,21 @@ public interface BotUtils {
 	IMessage sendMessageSync(IChannel channel, String message);
 
 	/**
+	 * Asynchronously sends a message. Will retry if a RateLimitException is
+	 * returned by Discord. Message will be deleted after the given amount of time.
+	 * 
+	 * @param channel
+	 *            The channel to send the message to. Non-null.
+	 * @param message
+	 *            The message to be sent. Must be non-null, non-empty.
+	 * @param long
+	 *            The delay before deleting the message.
+	 * @param timeUnit
+	 *            The unit of time for the {@code delay}. Non-null.
+	 */
+	void sendMessage(IChannel channel, String message, long delay, TimeUnit timeUnit);
+
+	/**
 	 * Asynchronously sends an embed object. Will retry if RateLimitException is
 	 * returned by Discord.
 	 * 
@@ -48,6 +64,21 @@ public interface BotUtils {
 	 */
 	void sendEmbed(IChannel channel, EmbedObject embedObj);
 	
+	/**
+	 * Asynchronously sends an embed object. Will retry if RateLimitException is
+	 * returned by Discord. Message will be deleted after the given amount of time.
+	 * 
+	 * @param channel
+	 *            The channel to send the message to. Non-null.
+	 * @param embedObj
+	 *            The embed object. Non-null.
+	 * @param long
+	 *            The delay before deleting the message.
+	 * @param timeUnit
+	 *            The unit of time for the {@code delay}. Non-null.
+	 */
+	void sendEmbed(IChannel channel, EmbedObject embedObj, long delay, TimeUnit timeUnit);
+
 	/**
 	 * Synchronously sends an embed object. Will retry if RateLimitException is
 	 * returned by Discord.
@@ -75,7 +106,7 @@ public interface BotUtils {
 	 *            alphanumeric. Non-null.
 	 */
 	void sendEmbed(IChannel channel, EmbedObject embedObj, InputStream imageFileStream, String fileName);
-	
+
 	/**
 	 * Synchronously sends an embed object with an image. Will retry if
 	 * RateLimitException is returned by Discord.
@@ -92,20 +123,22 @@ public interface BotUtils {
 	 * @return The sent embed message.
 	 */
 	IMessage sendEmbedSync(IChannel channel, EmbedObject embedObj, InputStream imageFileStream, String fileName);
-	
+
 	/**
-	 * Asynchronously and quietly deletes messages. Will retry if
-	 * RateLimitException is returned by Discord.
+	 * Asynchronously and quietly deletes messages. Will retry if RateLimitException
+	 * is returned by Discord.
 	 * 
-	 * @param messages The list of messages to delete. Non-null.
+	 * @param messages
+	 *            The list of messages to delete. Non-null.
 	 */
 	void deleteMessagesQuietly(List<IMessage> messages);
-	
+
 	/**
-	 * Asynchronously and quietly deletes messages. Will retry if
-	 * RateLimitException is returned by Discord.
+	 * Asynchronously and quietly deletes messages. Will retry if RateLimitException
+	 * is returned by Discord.
 	 * 
-	 * @param messages The messages to delete. Non-null.
+	 * @param messages
+	 *            The messages to delete. Non-null.
 	 */
 	void deleteMessagesQuietly(IMessage... message);
 }
